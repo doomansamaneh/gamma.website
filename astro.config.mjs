d0d69bd10805f40bbf2c3d7c442ccb6275393024
Mohammad Yengimolki <gammadesk@gmail.com>
2026-09-05 17:18:16 +0330

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro";
 
 export default defineConfig({
-  output: 'static',
-  site: 'https://example.com',
+  output: "static",
 });
