3f080a21668593f85908bba2d74f729fb65ca29e
Dooman Samane <35027789+doomansamaneh@users.noreply.github.com>
2026-09-05 16:34:28 +0330

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
-  output: 'static',
-  site: 'https://example.com',
+  output: "static",
+  adapter: cloudflare(),
 });
